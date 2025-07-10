05e6719e3e92886dc0d91d9a53236e2c6e2471f9
lucky-pradip <sahoopradip2005@gmail.com>
2025-07-10 07:15:01 +0000

    scope_01

---
+++ 01_half/21_scope.js
@@ -0,0 +1,18 @@
+let a=200
+
+if(true){
+    let a= 10;
+    const b=20;
+    c= 30;
+    console.log(a)
+
+}
+
+console.log(a)
+// console.log(b)
+// console.log(c)
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
