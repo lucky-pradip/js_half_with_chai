9643bbf8cc69aaca94b933dfb862fd1bdaa8379c
lucky-pradip <sahoopradip2005@gmail.com>
2025-07-10 07:38:24 +0000

    scope_02

---
+++ 01_half/22_scope_02.js
@@ -0,0 +1,33 @@
+function one (){
+    const userName="Prdaip"
+    function two(){
+        const website="youtube"
+        console.log(userName)
+    }
+    two()
+    // console.log(website)
+}
+// one()
+
+if(true){
+    const userName="Pradip"
+    if(userName==="Pradip"){
+        const website=" youtube"
+        // console.log(userName + website)
+    }
+    // console.log(website)
+}
+// console.log(userName)
+
+//++++++++++++//
+
+console.log(addone(5))
+function addone(num){
+    return num +1
+}
+
+
+addTwo(5)
+const addTwo=function(num){
+    return num+2
+}
